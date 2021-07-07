a538f1bc934388446ba1f81488eb2c4e5e5797f3
Mohammad Azam <azamsharp@gmail.com>
2021-07-06 20:47:02 -0500

    Initial Commit

---
+++ GreenGarden/GreenGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenGardenApp.swift
+//  GreenGarden
+//
+//  Created by Mohammad Azam on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GreenGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
